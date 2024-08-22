fc6bc600c6b80f14e261c003fc0378e7ce2dcdd2
Vincent Archambault-Bouffard <vincentarchambault@icloud.com>
2024-08-22 11:42:50 -0400

    Add parameter in report module to specify the name of the existing colums

--- src/brightsidebudget/report.py
+++ src/brightsidebudget/report.py
@@ -7,31 +7,33 @@ from typing import Literal, Union
 import polars as pl
 
 
-def add_year_column(df: pl.DataFrame, col_name: str = "Year") -> pl.DataFrame:
+def add_year_column(df: pl.DataFrame, col_name: str = "Year",
+                    date_col: str = "Date") -> pl.DataFrame:
     """
     Add a year column to the dataframe.
     """
     return df.with_columns(
-        pl.col("Date").dt.year().cast(pl.Int32).alias(col_name)
+        pl.col(date_col).dt.year().cast(pl.Int32).alias(col_name)
         )
 
 
 def add_month_column(df: pl.DataFrame,
                      col_name: str = "Month",
-                     month_type: Literal["number", "short", "long"] = "number"
+                     month_type: Literal["number", "short", "long"] = "number",
+                     date_col: str = "Date"
                      ) -> pl.DataFrame:
     """
     Add a month column to the dataframe.
     """
     if month_type == "number":
         dt = pl.Int32
-        expr = pl.col("Date").dt.month()
+        expr = pl.col(date_col).dt.month()
     elif month_type == "short":
         dt = pl.Utf8
-        expr = pl.col("Date").dt.strftime("%b")
+        expr = pl.col(date_col).dt.strftime("%b")
     elif month_type == "long":
         dt = pl.Utf8
-        expr = pl.col("Date").dt.strftime("%B")
+        expr = pl.col(date_col).dt.strftime("%B")
     else:
         raise ValueError(f"Invalid month type {month_type}")
     return df.with_columns(
@@ -39,26 +41,28 @@ def add_month_column(df: pl.DataFrame,
         )
 
 
-def add_year_month_column(df: pl.DataFrame, col_name: str = "Year-Month") -> pl.DataFrame:
+def add_year_month_column(df: pl.DataFrame, col_name: str = "Year-Month",
+                          date_col: str = "Date") -> pl.DataFrame:
     """
     Add a year-month (ex: 2024-01) column to the dataframe.
     """
     return df.with_columns(
-        pl.col("Date").dt.strftime("%Y-%m").cast(pl.Utf8).alias(col_name)
+        pl.col(date_col).dt.strftime("%Y-%m").cast(pl.Utf8).alias(col_name)
         )
 
 
 def add_fiscal_year_column(df: pl.DataFrame,
                            first_fiscal_month: int = 1,
-                           col_name: str = "Fiscal Year") -> pl.DataFrame:
+                           col_name: str = "Fiscal Year",
+                           date_col: str = "Date") -> pl.DataFrame:
     """
     Add a fiscal year column to the dataframe.
     """
     ffm = first_fiscal_month
     return df.with_columns(
-        pl.when((ffm == 1) | (pl.col("Date").dt.month() < ffm))
-        .then(pl.col("Date").dt.year())
-        .otherwise(pl.col("Date").dt.year() + 1)
+        pl.when((ffm == 1) | (pl.col(date_col).dt.month() < ffm))
+        .then(pl.col(date_col).dt.year())
+        .otherwise(pl.col(date_col).dt.year() + 1)
         .cast(pl.Int32)
         .alias(col_name)
         )
@@ -66,12 +70,13 @@ def add_fiscal_year_column(df: pl.DataFrame,
 
 def add_fiscal_month_column(df: pl.DataFrame,
                             first_fiscal_month: int = 1,
-                            col_name: str = "Fiscal Month") -> pl.DataFrame:
+                            col_name: str = "Fiscal Month",
+                            date_col: str = "Date") -> pl.DataFrame:
     """
     Add a fiscal month column to the dataframe.
     """
     return df.with_columns(
-        (((pl.col("Date").dt.month() - first_fiscal_month) % 12) + 1)
+        (((pl.col(date_col).dt.month() - first_fiscal_month) % 12) + 1)
         .cast(pl.Int32)
         .alias(col_name)
         )
@@ -79,25 +84,29 @@ def add_fiscal_month_column(df: pl.DataFrame,
 
 def add_relative_month_column(df: pl.DataFrame,
                               col_name: str = "Relative Month",
-                              today: Union[date, None] = None) -> pl.DataFrame:
+                              today: Union[date, None] = None,
+                              date_col: str = "Date") -> pl.DataFrame:
     """
     Add a relative month column to the dataframe.
     """
     if today is None:
         today = date.today()
     return df.with_columns(
-        ((pl.col("Date").dt.year() - today.year) * 12 + pl.col("Date").dt.month() - today.month)
+        ((pl.col(date_col).dt.year() - today.year) * 12 + pl.col(date_col).dt.month() - today.month)
         .cast(pl.Int32)
         .alias(col_name)
         )
 
 
 def add_txn_accounts_column(df: pl.DataFrame,
-                            col_name: str = "Txn Accounts") -> pl.DataFrame:
+                            col_name: str = "Txn Accounts",
+                            txn_col: str = "Txn",
+                            acc_name: str = "Account short name",
+                            delimiter: str = " | ") -> pl.DataFrame:
     """
     Add a column with the accounts involved in the transaction.
     """
-    grouped_df = df.group_by("Txn").agg(
-        pl.col('Account short name').unique().sort().str.concat(delimiter=" | ").alias(col_name)
+    grouped_df = df.group_by(txn_col).agg(
+        pl.col(acc_name).unique().sort().str.concat(delimiter=delimiter).alias(col_name)
     )
-    return df.join(grouped_df, on="Txn", how="left")
+    return df.join(grouped_df, on=txn_col, how="left")
